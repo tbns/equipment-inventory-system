cd2b865725851f4d9ea6524e8f2f80eb7e07d38f
Szymon Iwański <szymon.iwanski@hotmail.com>
2015-03-06 18:21:41 +0100

    fuck up manytomany

--- src/main/java/pl/tbns/model/Equipment.java
+++ src/main/java/pl/tbns/model/Equipment.java
@@ -4,6 +4,9 @@
 package pl.tbns.model;
 
 import java.io.Serializable;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
@@ -12,7 +15,8 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.Size;
 
@@ -47,8 +51,21 @@ public class Equipment implements Serializable {
 	@Size(min = 0, max = 2000)
 	private String description;
 	
+	@ManyToMany(mappedBy = "equipment")
+	private List<EquipmentsType> equipmentsType;
+	
 	public Equipment() {
 		
+	}
+
+	public Equipment(String name, String serialNumber, String equipmentsNumber,
+			String description, List<EquipmentsType> equipmentsType) {
+		super();
+		this.name = name;
+		this.serialNumber = serialNumber;
+		this.equipmentsNumber = equipmentsNumber;
+		this.description = description;
+		this.equipmentsType = equipmentsType;
 	}
 
 	public long getId() {
@@ -91,7 +108,70 @@ public class Equipment implements Serializable {
 		this.description = description;
 	}
 
-	
-	
+	public List<EquipmentsType> getEquipmentsType() {
+		return equipmentsType;
+	}
+
+	public void setEquipmentsType(List<EquipmentsType> equipmentsType) {
+		this.equipmentsType = equipmentsType;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result
+				+ ((description == null) ? 0 : description.hashCode());
+		result = prime
+				* result
+				+ ((equipmentsNumber == null) ? 0 : equipmentsNumber.hashCode());
+		result = prime * result
+				+ ((equipmentsType == null) ? 0 : equipmentsType.hashCode());
+		result = prime * result + (int) (id ^ (id >>> 32));
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result
+				+ ((serialNumber == null) ? 0 : serialNumber.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Equipment other = (Equipment) obj;
+		if (description == null) {
+			if (other.description != null)
+				return false;
+		} else if (!description.equals(other.description))
+			return false;
+		if (equipmentsNumber == null) {
+			if (other.equipmentsNumber != null)
+				return false;
+		} else if (!equipmentsNumber.equals(other.equipmentsNumber))
+			return false;
+		if (equipmentsType == null) {
+			if (other.equipmentsType != null)
+				return false;
+		} else if (!equipmentsType.equals(other.equipmentsType))
+			return false;
+		if (id != other.id)
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (serialNumber == null) {
+			if (other.serialNumber != null)
+				return false;
+		} else if (!serialNumber.equals(other.serialNumber))
+			return false;
+		return true;
+	}
+
 	
 }
